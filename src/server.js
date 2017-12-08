053821156cdbad583531cd71cd2c0b01e8077543
Giancarlo Anemone <ganemone@uber.com>
2017-12-07 17:34:11 -0800

    refactor into separate methods

--- src/server.js
+++ src/server.js
@@ -26,9 +26,62 @@ function tokenize(secret, salt) {
   return salt + '-' + base64Url.escape(hmac);
 }
 
+function loadOrGenerateSecret(session) {
+  let secret = session.get('csrf-secret');
+  if (!secret) {
+    secret = generateSecret();
+    session.set('csrf-secret', secret);
+  }
+  return secret;
+}
+
 export default ({Session, expire = 86400}) => {
   assert(Session, '{Session} is a required dependency of CsrfToken');
   const ignored = new Set();
+
+  function handleTokenPost(ctx, next) {
+    const session = Session.of(ctx);
+    const secret = loadOrGenerateSecret(session);
+    ctx.set('x-csrf-token', generateToken(secret));
+    ctx.status = 200;
+    ctx.body = '';
+    return next();
+  }
+
+  function handleSSR(ctx, next) {
+    const session = Session.of(ctx);
+    loadOrGenerateSecret(session);
+    return next();
+  }
+
+  async function checkCSRF(ctx, next) {
+    const session = Session.of(ctx);
+
+    const token = ctx.headers['x-csrf-token'];
+    const secret = session.get('csrf-secret');
+    const isMatchingToken = verifyToken(secret, token);
+    const isValidToken = verifyExpiry(token, expire);
+    if (!isMatchingToken || !isValidToken) {
+      await next();
+      ctx.status = 403;
+      if (__DEV__) {
+        // in development, missing CSRF tokens are most likely a developer mistake
+        // so we provide an explanation of how to fix it
+        ctx.status = 500;
+        ctx.body =
+          'CSRF Token configuration error: ' +
+          'add the option {fetch: CsrfToken.fetch} to ' +
+          'the 2nd argument of app.plugin(yourPlugin)';
+      } else {
+        // in production, provide a relevant error message
+        ctx.body = 'Invalid CSRF token';
+      }
+      if (ctx.headers.accept === 'application/json') {
+        ctx.body = JSON.stringify({error: ctx.body});
+      }
+    }
+  }
+
   return new Plugin({
     Service: class CsrfProtection {
       ignore(path) {
@@ -36,45 +89,12 @@ export default ({Session, expire = 86400}) => {
       }
     },
     async middleware(ctx, next) {
-      const session = Session.of(ctx);
-
-      if (
-        ctx.path === (ctx.prefix || '') + '/csrf-token' &&
-        ctx.method === 'POST'
-      ) {
-        let secret = session.get('csrf-secret');
-        if (!secret) {
-          secret = generateSecret();
-          session.set('csrf-secret', secret);
-        }
-        ctx.set('x-csrf-token', generateToken(secret));
-        await next();
-        ctx.status = 200;
-        ctx.body = '';
+      if (ctx.element) {
+        return handleSSR(ctx, next);
+      } else if (ctx.path === '/csrf-token' && ctx.method === 'POST') {
+        return handleTokenPost(ctx, next);
       } else if (verifyMethod(ctx.method) && !ignored.has(ctx.path)) {
-        const token = ctx.headers['x-csrf-token'];
-        const secret = session.get('csrf-secret');
-        const isMatchingToken = verifyToken(secret, token);
-        const isValidToken = verifyExpiry(token, expire);
-        if (!isMatchingToken || !isValidToken) {
-          await next();
-          ctx.status = 403;
-          if (__DEV__) {
-            // in development, missing CSRF tokens are most likely a developer mistake
-            // so we provide an explanation of how to fix it
-            ctx.status = 500;
-            ctx.body =
-              'CSRF Token configuration error: ' +
-              'add the option {fetch: CsrfToken.fetch} to ' +
-              'the 2nd argument of app.plugin(yourPlugin)';
-          } else {
-            // in production, provide a relevant error message
-            ctx.body = 'Invalid CSRF token';
-          }
-          if (ctx.headers.accept === 'application/json') {
-            ctx.body = JSON.stringify({error: ctx.body});
-          }
-        } else return next();
+        return checkCSRF(ctx, next);
       } else return next();
     },
   });
